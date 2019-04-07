29172f07478a2fa9bf911a958e2d5cd2e8ca2b57
LIM HYUN SEOK <cr3ux53c@gmail.com>
2019-04-07 17:51:39 +0900

    설정 파일 제외

--- knut_sugang/wsgi.py
+++ knut_sugang/wsgi.py
@@ -8,8 +8,11 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
+
+sys.path.append('C:/Apache24/htdocs/knut-sugang')
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'knut_sugang.settings')
 
