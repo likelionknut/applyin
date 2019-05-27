37f91217b0a62db95508f611d3fb1124dbc7f96c
LIM HYUN SEOK <cr3ux53c@gmail.com>
2019-05-28 03:21:32 +0900

    apply 뷰 작성

--- www/views.py
+++ www/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, HttpResponseRedirect
-from django.views.generic import ListView
+from django.shortcuts import render, HttpResponseRedirect, get_object_or_404, redirect
+from django.views.generic import View, ListView, DetailView
 from www.models import *
+from www.form import ApplicationForm
 
 
 # Create your views here.
@@ -8,6 +9,19 @@ from www.models import *
 class Welcome(ListView):
     model = Job
     template_name = 'www/index.html'
+
+
+class Apply(View):
+    def get(self, request, apply_dest):
+        application_form = ApplicationForm()
+        return render(request, 'www/apply.html', {'application_form': application_form})
+
+    def post(self, request, apply_dest):
+        form_data = ApplicationForm(data=request.POST)
+        if form_data.is_valid():
+            application = form_data.save(commit=False)
+            application.save()
+        return redirect('index')
 
 # def index(request):
 #     return render(request, 'www/index.html')
