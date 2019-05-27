e8aca2cfbcc5b1d447e8af9220f8a2d570ea95c6
LIM HYUN SEOK <cr3ux53c@gmail.com>
2019-05-28 03:19:13 +0900

    summernote URL 추가

--- applyin/urls.py
+++ applyin/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('management/', admin.site.urls),
-    path('', include('www.urls'))
+    path('admin/', admin.site.urls),
+    path('', include('www.urls')),
+    path('summernote/', include('django_summernote.urls')),
 ]
